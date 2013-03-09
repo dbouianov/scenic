b6907641b4267da7ad67ffffd3d24a52b73dff05
Dmitri Bouianov <dbouianov@gmail.com>
2013-03-08 23:51:14 -0800

    Tweaking Makefile, removing log files

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-export ROOT_DIR="$HOME/proj/scenic"
-export SRC_DIR="$ROOT_DIR/src"
-export BIN_DIR="$ROOT_DIR/bin"
-make -e -I "$ROOT_DIR/build" $@
+#!/bin/bash -e
+export ROOT_DIR=../
+export SRC_DIR=${ROOT_DIR}/src
+export BIN_DIR=${ROOT_DIR}/bin
+make -e -I ${ROOT_DIR}/build $@
